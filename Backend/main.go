55b0be2739531c80528d5cd767062967117df4ab
James Lanham <james.r.lanham2021@gmail.com>
2024-09-14 08:55:59 -0400

    Incorporating boiler plate code contributed from akmamun/chi-boilerplate with some modifications to fit this use case

--- Backend/main.go
+++ Backend/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-    "net/http"
+	"chi-boilerplate/config"
+	"chi-boilerplate/infra/database"
+	"chi-boilerplate/infra/logger"
+	"chi-boilerplate/migrations"
+	"chi-boilerplate/routers"
+	"net/http"
+	"time"
 
-    "github.com/go-chi/chi/v5"
-    "github.com/go-chi/chi/v5/middleware"
+	"github.com/spf13/viper"
 )
 
 func main() {
-    r := chi.NewRouter()
-    r.Use(middleware.Logger)
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hello World!"))
-    })
-    http.ListenAndServe(":3000", r)
+
+	//set timezone
+	viper.SetDefault("SERVER_TIMEZONE", "Asia/Dhaka")
+	loc, _ := time.LoadLocation(viper.GetString("SERVER_TIMEZONE"))
+	time.Local = loc
+
+	if err := config.SetupConfig(); err != nil {
+		logger.Fatalf("config SetupConfig() error: %s", err)
+	}
+
+	masterDSN, replicaDSN := config.DbConfiguration()
+	if err := database.DbConnection(masterDSN, replicaDSN); err != nil {
+		logger.Fatalf("database DbConnection error: %s", err)
+	}
+	//later separate migration
+	migrations.Migrate()
+
+	router := routers.SetupRoute()
+	logger.Fatalf("%v", http.ListenAndServe(config.ServerConfig(), router))
+
 }
