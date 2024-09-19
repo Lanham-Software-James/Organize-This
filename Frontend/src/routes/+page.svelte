5f929c9031c1182efb236876f6e5000d3ca0bfce
James Lanham <james.r.lanham2021@gmail.com>
2024-09-19 13:35:40 -0400

    Items page structure

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,15 +1,42 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import { Paginator, type PaginationSettings } from '@skeletonlabs/skeleton';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+	const source = [ /* any array of objects */ ];
+
+	let paginationSettings = {
+	page: 0,
+	limit: 5,
+	size: source.length,
+	amounts: [10, 25, 50, 100],
+} satisfies PaginationSettings;
+</script>
+
+<h2 class="pb-4 text-xl">Items</h2>
+<div class="flex flex-col justify-between">
+	<div class="table-container pb-4">
+		<table class="table table-compact table-hover">
+			<thead>
+				<tr>
+					<th class="!py-2">Name</th>
+					<th class="!py-2">Location</th>
+				</tr>
+			</thead>
+			<tbody>
+					<tr>
+						<td> </td>
+						<td> </td>
+					</tr>
+			</tbody>
+		</table>
 	</div>
+
+	<div>
+		<Paginator
+		bind:settings={paginationSettings}
+		showFirstLastButtons="{false}"
+		showPreviousNextButtons="{true}"
+		/>
+	</div>
+
 </div>
+
