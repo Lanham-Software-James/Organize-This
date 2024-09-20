115d2cb5a5f3e341628588e3a8901fa00a6005f6
James Lanham <james.r.lanham2021@gmail.com>
2024-09-20 16:19:54 -0400

    Fix modal overflow bug

--- Frontend/src/lib/AddNewModal/AddNewModal.svelte
+++ Frontend/src/lib/AddNewModal/AddNewModal.svelte
@@ -25,7 +25,7 @@
 	];
 
 	// Base Classes
-	const cBase = 'card p-4 w-modal shadow-xl space-y-4';
+	const cBase = 'card p-4 w-modal shadow-xl space-y-4 max-h-screen overflow-scroll';
 	const cHeader = 'text-2xl font-bold';
 	const cForm = 'border border-surface-500 p-4 space-y-4 rounded-container-token';
 
@@ -57,7 +57,7 @@
 </script>
 
 {#if $modalStore[0]}
-	<div class="modal-example-form {cBase}">
+	<div class="modal-add-entity-form {cBase}">
 		<header class={cHeader}>{$modalStore[0].title ?? '(title missing)'}</header>
 		<article>{$modalStore[0].body ?? '(body missing)'}</article>
 		<!-- Enable for debugging: -->
