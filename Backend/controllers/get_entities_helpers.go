c6b308383671e373005093b32a528211f45687cc
James Lanham <james.r.lanham2021@gmail.com>
2024-09-25 13:49:16 -0400

    Providing default values for offset and limit

--- Backend/controllers/get_entities_helpers.go
+++ Backend/controllers/get_entities_helpers.go
@@ -29,6 +29,14 @@ func getEntitiesParseQueryParams(values url.Values) (int, int, error) {
 	offsetString := values.Get("offset")
 	limitString := values.Get("limit")
 
+	if offsetString == "" {
+		offsetString = "0"
+	}
+
+	if limitString == "" {
+		limitString = "20"
+	}
+
 	offset, err := strconv.Atoi(offsetString)
 	if err != nil {
 		logger.Errorf("Error converting offset to int: %v", err)
