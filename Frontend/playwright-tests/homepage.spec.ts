e7c1c5a2637669fca7839f353a45f678ddbf0a59
James Lanham <james.r.lanham2021@gmail.com>
2024-09-26 17:33:52 -0400

    Missed some of the changes to the homepage

--- Frontend/playwright-tests/homepage.spec.ts
+++ Frontend/playwright-tests/homepage.spec.ts
@@ -12,11 +12,5 @@ test('INT-1: Homepage Has Expected HTML', async ({ page }) => {
 
 	await expect(page.locator('#filter')).toBeVisible();
 
-	await expect(page.locator('#th-name')).toBeVisible();
-	await expect(page.locator('#th-name')).toContainText("Name");
-
-	await expect(page.locator('#th-location')).toBeVisible();
-	await expect(page.locator('#th-location')).toContainText("Location");
-
 	await expect(page.locator('.paginator')).toBeVisible();
 });
