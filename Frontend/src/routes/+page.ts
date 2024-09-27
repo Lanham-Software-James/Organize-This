210de07cfbbcc4cc0c391e1a9e706c1985d2f7e6
James Lanham <james.r.lanham2021@gmail.com>
2024-09-27 19:51:16 -0400

    Adding error handling for getEntities

--- Frontend/src/routes/+page.ts
+++ Frontend/src/routes/+page.ts
@@ -17,13 +17,28 @@ export const _getEntities = async (offset: number, limit: number): Promise<[GetE
     let entities: GetEntitiesData[] = []
     let size: number = 0
 
-    const response = await fetch(`${PUBLIC_API_URL}v1/entities?offset=${offset}&limit=${limit}`);
+    try{
+        const response = await fetch(`${PUBLIC_API_URL}v1/entities?offset=${offset}&limit=${limit}`);
+        const data = await response.json()
 
-    const data = await response.json()
+        if (data.message == "success") {
+            entities = data.data.Entities
+            size = +data.data.TotalCount
+        }
 
-    if (data.message == "success") {
-        entities = data.data.Entities
-        size = +data.data.TotalCount
+    } catch(error) {
+        console.error(error);
+    }
+
+    // Give empty default row for table to iterate over
+    if(entities.length === 0) {
+        entities.push({
+            ID: 0,
+            Name: " ",
+            Category: " ",
+            Location: " ",
+            Notes: " "
+        })
     }
 
     return [entities, size]
