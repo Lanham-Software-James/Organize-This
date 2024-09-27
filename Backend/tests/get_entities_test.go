0f3751b2d3058a0c21ae78f5e6709be6fdb15ed0
James Lanham <james.r.lanham2021@gmail.com>
2024-09-26 20:45:07 -0400

    Updating unit test to match optimized queries

--- Backend/tests/get_entities_test.go
+++ Backend/tests/get_entities_test.go
@@ -27,22 +27,42 @@ func TestGetEntities(t *testing.T) {
 
 	t.Run("BEUT-18: Get Entities Valid URL Params", func(t *testing.T) {
 
-		mock.ExpectQuery(`SELECT \* FROM "buildings" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Building 1").AddRow(2, "Building 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "buildings"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+		mock.ExpectQuery(`SELECT 'building' AS category, id, name, notes, ' ' as location FROM buildings
+							UNION ALL
+							SELECT 'room' AS category, id, name, notes, ' ' as location FROM rooms
+							UNION ALL
+							SELECT 'shelving_unit' AS category, id, name, notes, ' ' as location FROM shelving_units
+							UNION ALL
+							SELECT 'shelf' AS category, id, name, notes, ' ' as location FROM shelves
+							UNION ALL
+							SELECT 'container' AS category, id, name, notes, ' ' as location FROM containers
+							UNION ALL
+							SELECT 'item' AS category, id, name, notes, ' ' as location FROM items
+							OFFSET 0
+							LIMIT 20`).
+			WillReturnRows(sqlmock.NewRows([]string{"category", "id", "name", "notes", "location"}).
+				AddRow("building", 1, "Building 1", "", " ").
+				AddRow("building", 2, "Building 2", "", " ").
+				AddRow("room", 1, "Room 1", "", " ").
+				AddRow("room", 2, "Room 2", "", " ").
+				AddRow("shelving_unit", 1, "Shelving Unit 1", "", " ").
+				AddRow("shelving_unit", 2, "Shelving Unit 2", "", " ").
+				AddRow("shelf", 1, "Shelf 1", "", " ").
+				AddRow("shelf", 2, "Shelf 2", "", " ").
+				AddRow("container", 1, "Container 1", "", " ").
+				AddRow("container", 2, "Container 2", "", " ").
+				AddRow("item", 1, "Item 1", "", " ").
+				AddRow("item", 2, "Item 2", "", " "))
 
-		mock.ExpectQuery(`SELECT \* FROM "rooms" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Room 1").AddRow(2, "Room 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "rooms"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelving_units" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelving Unit 1").AddRow(2, "Shelving Unit 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelving_units"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelves" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelf 1").AddRow(2, "Shelf 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelves"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "containers" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Container 1").AddRow(2, "Container 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "containers"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "items" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Item 1").AddRow(2, "Item 2"))
+		mock.ExpectQuery(`SELECT
+							(SELECT COUNT\(\*\) FROM buildings) \+
+							(SELECT COUNT\(\*\) FROM rooms) \+
+							(SELECT COUNT\(\*\) FROM shelving_units) \+
+							(SELECT COUNT\(\*\) FROM shelves) \+
+							(SELECT COUNT\(\*\) FROM containers) \+
+							(SELECT COUNT\(\*\) FROM items)
+						AS EntityCount`).
+			WillReturnRows(sqlmock.NewRows([]string{"EntityCount"}).AddRow(6))
 
 		params := "?offset=0&limit=20"
 		req := httptest.NewRequest(http.MethodGet, srv.URL+endpoint+params, nil)
@@ -59,22 +79,41 @@ func TestGetEntities(t *testing.T) {
 
 	t.Run("BEUT-19: Get Entities No Offset Param", func(t *testing.T) {
 
-		mock.ExpectQuery(`SELECT \* FROM "buildings" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Building 1").AddRow(2, "Building 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "buildings"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+		mock.ExpectQuery(`SELECT 'building' AS category, id, name, notes, ' ' as location FROM buildings
+							UNION ALL
+							SELECT 'room' AS category, id, name, notes, ' ' as location FROM rooms
+							UNION ALL
+							SELECT 'shelving_unit' AS category, id, name, notes, ' ' as location FROM shelving_units
+							UNION ALL
+							SELECT 'shelf' AS category, id, name, notes, ' ' as location FROM shelves
+							UNION ALL
+							SELECT 'container' AS category, id, name, notes, ' ' as location FROM containers
+							UNION ALL
+							SELECT 'item' AS category, id, name, notes, ' ' as location FROM items
+							LIMIT 20`).
+			WillReturnRows(sqlmock.NewRows([]string{"category", "id", "name", "notes", "location"}).
+				AddRow("building", 1, "Building 1", "", " ").
+				AddRow("building", 2, "Building 2", "", " ").
+				AddRow("room", 1, "Room 1", "", " ").
+				AddRow("room", 2, "Room 2", "", " ").
+				AddRow("shelving_unit", 1, "Shelving Unit 1", "", " ").
+				AddRow("shelving_unit", 2, "Shelving Unit 2", "", " ").
+				AddRow("shelf", 1, "Shelf 1", "", " ").
+				AddRow("shelf", 2, "Shelf 2", "", " ").
+				AddRow("container", 1, "Container 1", "", " ").
+				AddRow("container", 2, "Container 2", "", " ").
+				AddRow("item", 1, "Item 1", "", " ").
+				AddRow("item", 2, "Item 2", "", " "))
 
-		mock.ExpectQuery(`SELECT \* FROM "rooms" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Room 1").AddRow(2, "Room 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "rooms"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelving_units" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelving Unit 1").AddRow(2, "Shelving Unit 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelving_units"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelves" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelf 1").AddRow(2, "Shelf 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelves"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "containers" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Container 1").AddRow(2, "Container 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "containers"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "items" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Item 1").AddRow(2, "Item 2"))
+		mock.ExpectQuery(`SELECT
+				(SELECT COUNT\(\*\) FROM buildings) \+
+				(SELECT COUNT\(\*\) FROM rooms) \+
+				(SELECT COUNT\(\*\) FROM shelving_units) \+
+				(SELECT COUNT\(\*\) FROM shelves) \+
+				(SELECT COUNT\(\*\) FROM containers) \+
+				(SELECT COUNT\(\*\) FROM items)
+			AS EntityCount`).
+			WillReturnRows(sqlmock.NewRows([]string{"EntityCount"}).AddRow(6))
 
 		params := "?limit=20"
 		req := httptest.NewRequest(http.MethodGet, srv.URL+endpoint+params, nil)
@@ -91,22 +130,41 @@ func TestGetEntities(t *testing.T) {
 
 	t.Run("BEUT-20: Get Entities No Limit Param", func(t *testing.T) {
 
-		mock.ExpectQuery(`SELECT \* FROM "buildings" LIMIT 20`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Building 1").AddRow(2, "Building 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "buildings"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+		mock.ExpectQuery(`SELECT 'building' AS category, id, name, notes, ' ' as location FROM buildings
+							UNION ALL
+							SELECT 'room' AS category, id, name, notes, ' ' as location FROM rooms
+							UNION ALL
+							SELECT 'shelving_unit' AS category, id, name, notes, ' ' as location FROM shelving_units
+							UNION ALL
+							SELECT 'shelf' AS category, id, name, notes, ' ' as location FROM shelves
+							UNION ALL
+							SELECT 'container' AS category, id, name, notes, ' ' as location FROM containers
+							UNION ALL
+							SELECT 'item' AS category, id, name, notes, ' ' as location FROM items
+							LIMIT 20`).
+			WillReturnRows(sqlmock.NewRows([]string{"category", "id", "name", "notes", "location"}).
+				AddRow("building", 1, "Building 1", "", " ").
+				AddRow("building", 2, "Building 2", "", " ").
+				AddRow("room", 1, "Room 1", "", " ").
+				AddRow("room", 2, "Room 2", "", " ").
+				AddRow("shelving_unit", 1, "Shelving Unit 1", "", " ").
+				AddRow("shelving_unit", 2, "Shelving Unit 2", "", " ").
+				AddRow("shelf", 1, "Shelf 1", "", " ").
+				AddRow("shelf", 2, "Shelf 2", "", " ").
+				AddRow("container", 1, "Container 1", "", " ").
+				AddRow("container", 2, "Container 2", "", " ").
+				AddRow("item", 1, "Item 1", "", " ").
+				AddRow("item", 2, "Item 2", "", " "))
 
-		mock.ExpectQuery(`SELECT \* FROM "rooms" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Room 1").AddRow(2, "Room 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "rooms"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelving_units" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelving Unit 1").AddRow(2, "Shelving Unit 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelving_units"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelves" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelf 1").AddRow(2, "Shelf 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelves"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "containers" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Container 1").AddRow(2, "Container 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "containers"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "items" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Item 1").AddRow(2, "Item 2"))
+		mock.ExpectQuery(`SELECT
+							(SELECT COUNT\(\*\) FROM buildings) \+
+							(SELECT COUNT\(\*\) FROM rooms) \+
+							(SELECT COUNT\(\*\) FROM shelving_units) \+
+							(SELECT COUNT\(\*\) FROM shelves) \+
+							(SELECT COUNT\(\*\) FROM containers) \+
+							(SELECT COUNT\(\*\) FROM items)
+						AS EntityCount`).
+			WillReturnRows(sqlmock.NewRows([]string{"EntityCount"}).AddRow(6))
 
 		params := "?offset=0"
 		req := httptest.NewRequest(http.MethodGet, srv.URL+endpoint+params, nil)
@@ -123,22 +181,42 @@ func TestGetEntities(t *testing.T) {
 
 	t.Run("BEUT-21: Get Entities No Params", func(t *testing.T) {
 
-		mock.ExpectQuery(`SELECT \* FROM "buildings" LIMIT 20`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Building 1").AddRow(2, "Building 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "buildings"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+		mock.ExpectQuery(`SELECT 'building' AS category, id, name, notes, ' ' as location FROM buildings
+							UNION ALL
+							SELECT 'room' AS category, id, name, notes, ' ' as location FROM rooms
+							UNION ALL
+							SELECT 'shelving_unit' AS category, id, name, notes, ' ' as location FROM shelving_units
+							UNION ALL
+							SELECT 'shelf' AS category, id, name, notes, ' ' as location FROM shelves
+							UNION ALL
+							SELECT 'container' AS category, id, name, notes, ' ' as location FROM containers
+							UNION ALL
+							SELECT 'item' AS category, id, name, notes, ' ' as location FROM items
+							OFFSET 0
+							LIMIT 20`).
+			WillReturnRows(sqlmock.NewRows([]string{"category", "id", "name", "notes", "location"}).
+				AddRow("building", 1, "Building 1", "", " ").
+				AddRow("building", 2, "Building 2", "", " ").
+				AddRow("room", 1, "Room 1", "", " ").
+				AddRow("room", 2, "Room 2", "", " ").
+				AddRow("shelving_unit", 1, "Shelving Unit 1", "", " ").
+				AddRow("shelving_unit", 2, "Shelving Unit 2", "", " ").
+				AddRow("shelf", 1, "Shelf 1", "", " ").
+				AddRow("shelf", 2, "Shelf 2", "", " ").
+				AddRow("container", 1, "Container 1", "", " ").
+				AddRow("container", 2, "Container 2", "", " ").
+				AddRow("item", 1, "Item 1", "", " ").
+				AddRow("item", 2, "Item 2", "", " "))
 
-		mock.ExpectQuery(`SELECT \* FROM "rooms" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Room 1").AddRow(2, "Room 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "rooms"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelving_units" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelving Unit 1").AddRow(2, "Shelving Unit 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelving_units"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelves" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelf 1").AddRow(2, "Shelf 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelves"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "containers" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Container 1").AddRow(2, "Container 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "containers"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "items" LIMIT \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Item 1").AddRow(2, "Item 2"))
+		mock.ExpectQuery(`SELECT
+							(SELECT COUNT\(\*\) FROM buildings) \+
+							(SELECT COUNT\(\*\) FROM rooms) \+
+							(SELECT COUNT\(\*\) FROM shelving_units) \+
+							(SELECT COUNT\(\*\) FROM shelves) \+
+							(SELECT COUNT\(\*\) FROM containers) \+
+							(SELECT COUNT\(\*\) FROM items)
+						AS EntityCount`).
+			WillReturnRows(sqlmock.NewRows([]string{"EntityCount"}).AddRow(6))
 
 		params := ""
 		req := httptest.NewRequest(http.MethodGet, srv.URL+endpoint+params, nil)
@@ -155,22 +233,42 @@ func TestGetEntities(t *testing.T) {
 
 	t.Run("BEUT-22: Get Entities Non-Zero Offset", func(t *testing.T) {
 
-		mock.ExpectQuery(`SELECT \* FROM "buildings" LIMIT \d+ OFFSET 20`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Building 1").AddRow(2, "Building 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "buildings"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+		mock.ExpectQuery(`SELECT 'building' AS category, id, name, notes, ' ' as location FROM buildings
+							UNION ALL
+							SELECT 'room' AS category, id, name, notes, ' ' as location FROM rooms
+							UNION ALL
+							SELECT 'shelving_unit' AS category, id, name, notes, ' ' as location FROM shelving_units
+							UNION ALL
+							SELECT 'shelf' AS category, id, name, notes, ' ' as location FROM shelves
+							UNION ALL
+							SELECT 'container' AS category, id, name, notes, ' ' as location FROM containers
+							UNION ALL
+							SELECT 'item' AS category, id, name, notes, ' ' as location FROM items
+							OFFSET 20
+							LIMIT 20`).
+			WillReturnRows(sqlmock.NewRows([]string{"category", "id", "name", "notes", "location"}).
+				AddRow("building", 1, "Building 1", "", " ").
+				AddRow("building", 2, "Building 2", "", " ").
+				AddRow("room", 1, "Room 1", "", " ").
+				AddRow("room", 2, "Room 2", "", " ").
+				AddRow("shelving_unit", 1, "Shelving Unit 1", "", " ").
+				AddRow("shelving_unit", 2, "Shelving Unit 2", "", " ").
+				AddRow("shelf", 1, "Shelf 1", "", " ").
+				AddRow("shelf", 2, "Shelf 2", "", " ").
+				AddRow("container", 1, "Container 1", "", " ").
+				AddRow("container", 2, "Container 2", "", " ").
+				AddRow("item", 1, "Item 1", "", " ").
+				AddRow("item", 2, "Item 2", "", " "))
 
-		mock.ExpectQuery(`SELECT \* FROM "rooms" LIMIT \d+ OFFSET \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Room 1").AddRow(2, "Room 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "rooms"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelving_units" LIMIT \d+ OFFSET \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelving Unit 1").AddRow(2, "Shelving Unit 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelving_units"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "shelves" LIMIT \d+ OFFSET \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Shelf 1").AddRow(2, "Shelf 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "shelves"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "containers" LIMIT \d+ OFFSET \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Container 1").AddRow(2, "Container 2"))
-		mock.ExpectQuery(`SELECT count\(\*\) FROM "containers"`).WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
-
-		mock.ExpectQuery(`SELECT \* FROM "items" LIMIT \d+ OFFSET \d+`).WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "Item 1").AddRow(2, "Item 2"))
+		mock.ExpectQuery(`SELECT
+							(SELECT COUNT\(\*\) FROM buildings) \+
+							(SELECT COUNT\(\*\) FROM rooms) \+
+							(SELECT COUNT\(\*\) FROM shelving_units) \+
+							(SELECT COUNT\(\*\) FROM shelves) \+
+							(SELECT COUNT\(\*\) FROM containers) \+
+							(SELECT COUNT\(\*\) FROM items)
+						AS EntityCount`).
+			WillReturnRows(sqlmock.NewRows([]string{"EntityCount"}).AddRow(6))
 
 		params := "?offset=20&limit=20"
 		req := httptest.NewRequest(http.MethodGet, srv.URL+endpoint+params, nil)
