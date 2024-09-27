4b6d7b76cb7f6a1f6e55d2513411e0257c734ec0
James Lanham <james.r.lanham2021@gmail.com>
2024-09-27 19:51:45 -0400

    Adding additional integration tests for the add new modal

--- Frontend/playwright-tests/addNewModal.spec.ts
+++ Frontend/playwright-tests/addNewModal.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('INT-3: Add New Modal Has Expected HTML and Loads Properly', async ({ page }) => {
+test('INT-3: Add New Modal Has Expected Shell HTML', async ({ page }) => {
   await page.goto('/');
 
   await expect(page.locator('.modal-add-entity-form')).toBeHidden();
@@ -18,13 +18,12 @@ test('INT-3: Add New Modal Has Expected HTML and Loads Properly', async ({ page 
   await expect(page.locator('label[for=category]')).toContainText("Category");
   await expect(page.locator('select#category')).toBeVisible();
 
-  await expect(page.locator('label[for=name]')).toBeVisible();
-  await expect(page.locator('label[for=name]')).toContainText("Name");
-  await expect(page.locator('input#name')).toBeVisible();
-
-  await expect(page.locator('label[for=notes]')).toBeVisible();
-  await expect(page.locator('label[for=notes]')).toContainText("Notes");
-  await expect(page.locator('textarea#notes')).toBeVisible();
+  await expect(page.locator('option[value="item"]')).toBeDefined();
+	await expect(page.locator('option[value="category"]')).toBeDefined();
+	await expect(page.locator('option[value="shelf"]')).toBeDefined();
+	await expect(page.locator('option[value="shelvingunit"]')).toBeDefined();
+	await expect(page.locator('option[value="room"]')).toBeDefined();
+  await expect(page.locator('option[value="building"]')).toBeDefined();
 
   await expect(page.locator('footer > button.variant-ghost-surface')).toBeVisible();
   await expect(page.locator('footer > button.variant-ghost-surface')).toContainText("Cancel");
@@ -32,3 +31,130 @@ test('INT-3: Add New Modal Has Expected HTML and Loads Properly', async ({ page 
   await expect(page.locator('footer > button.variant-filled')).toBeVisible();
   await expect(page.locator('footer > button.variant-filled')).toContainText("Submit");
 });
+
+test('INT-4: Add New Modal Item Category Has Fields', async ({ page }) => {
+  await page.goto('/');
+
+  // Load Nav Bar
+  await page.click('#hamburgerMenu');
+
+  // Load Add New Modal
+  await page.getByRole('button', { name: '+ Add New' }).click();
+
+  await expect(page.locator('label[for=name]')).toBeVisible();
+  await expect(page.locator('label[for=name]')).toContainText("Name");
+  await expect(page.locator('input#name')).toBeVisible();
+
+  await expect(page.locator('label[for=notes]')).toBeVisible();
+  await expect(page.locator('label[for=notes]')).toContainText("Notes");
+  await expect(page.locator('textarea#notes')).toBeVisible();
+})
+
+test('INT-5: Add New Modal Container Category Has Fields', async ({ page }) => {
+  await page.goto('/');
+
+  // Load Nav Bar
+  await page.click('#hamburgerMenu');
+
+  // Load Add New Modal
+  await page.getByRole('button', { name: '+ Add New' }).click();
+
+  // Select Different Category
+  await page.getByLabel('Category:').selectOption('container');
+
+  await expect(page.locator('label[for=name]')).toBeVisible();
+  await expect(page.locator('label[for=name]')).toContainText("Name");
+  await expect(page.locator('input#name')).toBeVisible();
+
+  await expect(page.locator('label[for=notes]')).toBeVisible();
+  await expect(page.locator('label[for=notes]')).toContainText("Notes");
+  await expect(page.locator('textarea#notes')).toBeVisible();
+})
+
+test('INT-6: Add New Modal Shelf Category Has Fields', async ({ page }) => {
+  await page.goto('/');
+
+  // Load Nav Bar
+  await page.click('#hamburgerMenu');
+
+  // Load Add New Modal
+  await page.getByRole('button', { name: '+ Add New' }).click();
+
+  // Select Different Category
+  await page.getByLabel('Category:').selectOption('shelf');
+
+  await expect(page.locator('label[for=name]')).toBeVisible();
+  await expect(page.locator('label[for=name]')).toContainText("Name");
+  await expect(page.locator('input#name')).toBeVisible();
+
+  await expect(page.locator('label[for=notes]')).toBeVisible();
+  await expect(page.locator('label[for=notes]')).toContainText("Notes");
+  await expect(page.locator('textarea#notes')).toBeVisible();
+})
+
+test('INT-7: Add New Modal Shelving Unit Category Has Fields', async ({ page }) => {
+  await page.goto('/');
+
+  // Load Nav Bar
+  await page.click('#hamburgerMenu');
+
+  // Load Add New Modal
+  await page.getByRole('button', { name: '+ Add New' }).click();
+
+  // Select Different Category
+  await page.getByLabel('Category:').selectOption('shelvingunit');
+
+  await expect(page.locator('label[for=name]')).toBeVisible();
+  await expect(page.locator('label[for=name]')).toContainText("Name");
+  await expect(page.locator('input#name')).toBeVisible();
+
+  await expect(page.locator('label[for=notes]')).toBeVisible();
+  await expect(page.locator('label[for=notes]')).toContainText("Notes");
+  await expect(page.locator('textarea#notes')).toBeVisible();
+})
+
+test('INT-8: Add New Modal Room Category Has Fields', async ({ page }) => {
+  await page.goto('/');
+
+  // Load Nav Bar
+  await page.click('#hamburgerMenu');
+
+  // Load Add New Modal
+  await page.getByRole('button', { name: '+ Add New' }).click();
+
+  // Select Different Category
+  await page.getByLabel('Category:').selectOption('room');
+
+  await expect(page.locator('label[for=name]')).toBeVisible();
+  await expect(page.locator('label[for=name]')).toContainText("Name");
+  await expect(page.locator('input#name')).toBeVisible();
+
+  await expect(page.locator('label[for=notes]')).toBeVisible();
+  await expect(page.locator('label[for=notes]')).toContainText("Notes");
+  await expect(page.locator('textarea#notes')).toBeVisible();
+})
+
+test('INT-9: Add New Modal Shelf Building Has Fields', async ({ page }) => {
+  await page.goto('/');
+
+  // Load Nav Bar
+  await page.click('#hamburgerMenu');
+
+  // Load Add New Modal
+  await page.getByRole('button', { name: '+ Add New' }).click();
+
+  // Select Different Category
+  await page.getByLabel('Category:').selectOption('building');
+
+  await expect(page.locator('label[for=name]')).toBeVisible();
+  await expect(page.locator('label[for=name]')).toContainText("Name");
+  await expect(page.locator('input#name')).toBeVisible();
+
+  await expect(page.locator('label[for=address]')).toBeVisible();
+  await expect(page.locator('label[for=address]')).toContainText("Address");
+  await expect(page.locator('input#address')).toBeVisible();
+
+  await expect(page.locator('label[for=notes]')).toBeVisible();
+  await expect(page.locator('label[for=notes]')).toContainText("Notes");
+  await expect(page.locator('textarea#notes')).toBeVisible();
+})
