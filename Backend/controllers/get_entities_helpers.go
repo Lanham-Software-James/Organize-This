aadf30436767d4d3d84f0d0135519c8470272359
James Lanham <james.r.lanham2021@gmail.com>
2024-10-31 06:53:45 -0400

    Search functionality backend

--- Backend/controllers/get_entities_helpers.go
+++ Backend/controllers/get_entities_helpers.go
@@ -7,9 +7,10 @@ import (
 	"strconv"
 )
 
-func getEntitiesParseQueryParams(values url.Values) (int, int, error) {
+func getEntitiesParseQueryParams(values url.Values) (int, int, string, error) {
 	offsetString := values.Get("offset")
 	limitString := values.Get("limit")
+	search := values.Get("search")
 
 	if offsetString == "" {
 		offsetString = "0"
@@ -22,26 +23,26 @@ func getEntitiesParseQueryParams(values url.Values) (int, int, error) {
 	offset, err := strconv.Atoi(offsetString)
 	if err != nil {
 		logger.Errorf("Error converting offset to int: %v", err)
-		return 0, 0, err
+		return 0, 0, "", err
 	}
 
 	limit, err := strconv.Atoi(limitString)
 	if err != nil {
 		logger.Errorf("Error converting limit to int: %v", err)
-		return 0, 0, err
+		return 0, 0, "", err
 	}
 
 	if offset < 0 {
 		err = errors.New("offset must be positive")
 		logger.Errorf("Error: %v", err)
-		return 0, 0, err
+		return 0, 0, "", err
 	}
 
 	if limit < 0 {
 		err = errors.New("limit must be positive")
 		logger.Errorf("Error: %v", err)
-		return 0, 0, err
+		return 0, 0, "", err
 	}
 
-	return offset, limit, nil
+	return offset, limit, search, nil
 }
