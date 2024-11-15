27f22a777e73ca760cbf457aaefc356d4b65ee55
James Lanham <james.r.lanham2021@gmail.com>
2024-11-15 10:03:19 -0500

    Adding QR code generation icon

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	import { Paginator, type PaginationSettings, getModalStore, popup, type ModalSettings, type PopupSettings } from '@skeletonlabs/skeleton';
+	import {
+		Paginator,
+		type PaginationSettings,
+		getModalStore,
+		popup,
+		type ModalSettings,
+		type PopupSettings
+	} from '@skeletonlabs/skeleton';
 	import { getContext, onMount } from 'svelte';
 	import { _getEntities as getEntities, type GetEntitiesData } from './+page';
 	import AddNewModal from '$lib/AddNewModal/AddNewModal.svelte';
@@ -22,11 +29,11 @@
 		loadData();
 
 		//@ts-ignore
-        const unsubscribe = refreshPage.subscribe(() => {
-            loadData();
-        });
+		const unsubscribe = refreshPage.subscribe(() => {
+			loadData();
+		});
 
-        return unsubscribe;
+		return unsubscribe;
 	});
 
 	async function loadData() {
@@ -52,35 +59,53 @@
 
 	const modalStore = getModalStore();
 
-	async function editEntity(id: number, category: string) {
-		const modal: ModalSettings = {
-			type: 'component',
-			component: {ref: AddNewModal, props: {edit: true, id: id, category: category, displayCategory: cleanCategory(category)}},
-			title: 'Edit Entity',
-			body: 'Please complete the form to edit an existing item, container, shelf, shelving unit, room, or building.'
-		};
-		modalStore.trigger(modal);
+	async function rowClick(event: MouseEvent, id: number, category: string) {
+		const target = event.target as HTMLElement;
+
+		if (
+			(target.tagName === 'TD' && target.classList.contains('qr')) ||
+			(target.tagName === 'I' && target.classList.contains('fa-qrcode'))
+		) {
+			// Display QR Modal
+		} else {
+			// Display Edit Modal
+			const modal: ModalSettings = {
+				type: 'component',
+				component: {
+					ref: AddNewModal,
+					props: {
+						edit: true,
+						id: id,
+						category: category,
+						displayCategory: cleanCategory(category)
+					}
+				},
+				title: 'Edit Entity',
+				body: 'Please complete the form to edit an existing item, container, shelf, shelving unit, room, or building.'
+			};
+			modalStore.trigger(modal);
+		}
 	}
 
 	function cleanCategory(category: string): string {
 		var cleanedCategory = category;
-		if(cleanedCategory == "shelving_unit"){
-			cleanedCategory = "shelving unit"
+		if (cleanedCategory == 'shelving_unit') {
+			cleanedCategory = 'shelving unit';
 		}
 
-		return cleanedCategory
+		return cleanedCategory;
 	}
 
-	let filters: {[key: string]: boolean} = {
-		'building': true,
-		'room': true,
-		'shelving_unit': true,
-		'shelf': true,
-		'container': true,
-		'item': true,
-	}
+	let filters: { [key: string]: boolean } = {
+		building: true,
+		room: true,
+		shelving_unit: true,
+		shelf: true,
+		container: true,
+		item: true
+	};
 
-	let search = ''
+	let search = '';
 
 	let isVisible = false;
 
@@ -92,8 +117,8 @@
 	};
 
 	function popUpOpenClose(e: Record<string, boolean>) {
-		if(!e.state) {
-			searchFilter()
+		if (!e.state) {
+			searchFilter();
 		}
 	}
 
@@ -104,32 +129,32 @@
 
 <div class="card p-4 w-72 shadow-xl" data-popup="popupFeatured">
 	<label class="flex items-center space-x-2">
-		<input class="checkbox" type="checkbox" bind:checked={filters['building']}/>
+		<input class="checkbox" type="checkbox" bind:checked={filters['building']} />
 		<p>Buildings</p>
 	</label>
 
 	<label class="flex items-center space-x-2">
-		<input class="checkbox" type="checkbox" bind:checked={filters['room']}/>
+		<input class="checkbox" type="checkbox" bind:checked={filters['room']} />
 		<p>Rooms</p>
 	</label>
 
 	<label class="flex items-center space-x-2">
-		<input class="checkbox" type="checkbox" bind:checked={filters['shelving_unit']}/>
+		<input class="checkbox" type="checkbox" bind:checked={filters['shelving_unit']} />
 		<p>Shelving Units</p>
 	</label>
 
 	<label class="flex items-center space-x-2">
-		<input class="checkbox" type="checkbox" bind:checked={filters['shelf']}/>
+		<input class="checkbox" type="checkbox" bind:checked={filters['shelf']} />
 		<p>Shelves</p>
 	</label>
 
 	<label class="flex items-center space-x-2">
-		<input class="checkbox" type="checkbox" bind:checked={filters['container']}/>
+		<input class="checkbox" type="checkbox" bind:checked={filters['container']} />
 		<p>Containers</p>
 	</label>
 
 	<label class="flex items-center space-x-2">
-		<input class="checkbox" type="checkbox" bind:checked={filters['item']}/>
+		<input class="checkbox" type="checkbox" bind:checked={filters['item']} />
 		<p>Items</p>
 	</label>
 </div>
@@ -145,16 +170,26 @@
 				type="text"
 				bind:value={search}
 				placeholder="search"
-				on:blur = {searchFilter}
+				on:blur={searchFilter}
 			/>
 		{/if}
 
 		<div class="flex flex-row justify-evenly items-center w-1/4">
-			<button id="filter" type="button" class="btn-icon btn-icon-sm variant-filled" on:click={toggleSearch}>
+			<button
+				id="filter"
+				type="button"
+				class="btn-icon btn-icon-sm variant-filled"
+				on:click={toggleSearch}
+			>
 				<i class="fa-solid fa-magnifying-glass fa-s"></i>
 			</button>
 
-			<button id="filter" type="button" class="btn-icon btn-icon-sm variant-filled" use:popup={popupFeatured}>
+			<button
+				id="filter"
+				type="button"
+				class="btn-icon btn-icon-sm variant-filled"
+				use:popup={popupFeatured}
+			>
 				<i class="fa-solid fa-filter fa-xs"></i>
 			</button>
 		</div>
@@ -168,16 +203,17 @@
 				<thead>
 					<tr>
 						<th id="th-name" class="!py-2">Name</th>
-						<th id="th-category" class="!py-2 hidden md:block">Category</th>
+						<th id="th-category" class="!py-2 invisible md:visible">Category</th>
 						<th id="th-location" class="!py-2">Location</th>
-						<th id="th-notes" class="!py-2 hidden lg:block">Notes</th>
+						<th id="th-notes" class="!py-2 invisible lg:visible">Notes</th>
+						<th>&nbsp;</th>
 					</tr>
 				</thead>
 				<tbody>
 					{#each entities as entity}
-						<tr on:click={() => editEntity(entity.ID, entity.Category)}>
+						<tr on:click={(event) => rowClick(event, entity.ID, entity.Category)}>
 							<td class="capitalize">{entity.Name}</td>
-							<td class="hidden md:block capitalize">{cleanCategory(entity.Category)}</td>
+							<td class="invisible md:visible capitalize">{cleanCategory(entity.Category)}</td>
 							<td>
 								{#if entity.Category == 'building'}
 									{entity.Address}
@@ -191,7 +227,12 @@
 									{/each}
 								{/if}
 							</td>
-							<td class="hidden lg:block">{entity.Notes ?? ""}</td>
+							<td class="invisible lg:visible">{entity.Notes ?? ''}</td>
+							<td class="qr">
+								<button type="button">
+									<i class="fa-solid fa-qrcode"></i>
+								</button>
+							</td>
 						</tr>
 					{/each}
 				</tbody>
