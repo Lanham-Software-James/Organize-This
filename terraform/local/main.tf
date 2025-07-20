a12524b1afa99cea8fb3150b231a168f7d7e16ac
James Lanham <james.r.lanham2021@gmail.com>
2025-07-20 13:13:43 -0400

    Add S3 bucket resource and related outputs to Terraform configuration

---
+++ terraform/local/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  profile = var.aws_profile
+
+  default_tags {
+    tags = {
+      Project     = var.project_readable_name
+      ManagedBy   = "Terraform"
+      Environment = var.environment
+    }
+  }
+}
+
+module "s3" {
+  source       = "./s3"
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+module "cognito" {
+  source                = "./cognito"
+  project_readable_name = var.project_readable_name
+  project_name          = var.project_name
+  environment           = var.environment
+}
+
+module "iam" {
+  source       = "./iam"
+  project_name = var.project_name
+  environment  = var.environment
+  region       = var.region
+  account_id   = var.account_id
+  user_pool_id = module.cognito.user_pool_id
+}
